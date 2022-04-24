f0eb142a0a4d92a1d0dd3e10de709c8e70702253
casmpy <navjotsingh301103@gmail.com>
2022-04-24 23:30:23 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './components/Home'
+import Upload from './components/Upload'
+
+function App() {
+ 
+
+  return (
+    <div className='bg-[#03203C] min-h-screen'>
+    <Home/>
+    <Upload/>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/Home.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Home() {
+	return (
+		<div className="flex justify-center">
+			<div>
+				<h1
+					className="
+			font-Lobster
+			tracking-wide
+			text-white
+			text-5xl
+		    font-extrabold
+			my-20
+			mx-40
+			"
+
+				>
+					METAMOD
+				</h1>
+			</div>
+			
+		</div>
+	);
+}
+
+export default Home;
